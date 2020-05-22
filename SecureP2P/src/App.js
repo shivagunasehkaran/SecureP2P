e94b922423f2680e1dd3c3b9ab6b8fa84f9f0f90
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2020-05-22 15:32:57 +0530

    Folder structure

---
+++ SecureP2P/src/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>{'Secure P2P'}</Text>
+    </View>
+  );
+};
+
+export default App;

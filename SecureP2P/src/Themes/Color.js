28fdea8eb7b1031c0ed88d725837b0bcbfdec13a
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2020-05-24 01:18:18 +0530

    Generalise colors for styles

---
+++ SecureP2P/src/Themes/Color.js
@@ -0,0 +1,11 @@
+const colors = {
+    appColor: '#D96F9E',
+    appBackGround: '#daeaed',
+    errorMessage: '#f02424',
+    borderColor: '#718185',
+    grayShade: '#CCCCCC',
+    black: '#000',
+    white: '#fff',
+};
+
+export default colors;

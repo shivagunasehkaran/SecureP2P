53f12e5ba874366a3aa399624605a03bc61b8d70
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2020-05-24 01:18:59 +0530

    Styles updated

--- SecureP2P/src/Screens/Styles/SignUp.style.js
+++ SecureP2P/src/Screens/Styles/SignUp.style.js
@@ -1,40 +1,42 @@
 import { StyleSheet } from 'react-native';
+import Colors from '../../Themes/Color';
+import Fonts from '../../Themes/Fonts';
 
 export const styles = StyleSheet.create({
     header: {
         width: '100%',
         height: 150,
-        backgroundColor: '#8541B0'
+        backgroundColor: Colors.appColor
     },
     headerTitle: {
-        color: '#fff',
+        color: Colors.white,
         fontSize: 20,
-        fontFamily: 'Avenir-Black',
+        fontFamily: Fonts.FONT_BLACK,
         marginTop: 50,
         paddingLeft: 30
     },
     headerSubTitle: {
-        color: '#fff',
+        color: Colors.white,
         fontSize: 15,
-        fontFamily: 'Avenir-Black',
+        fontFamily: Fonts.FONT_BLACK,
         paddingLeft: 30,
         paddingTop: 10
     },
     cententer: {
         width: '100%',
         height: 600,
-        backgroundColor: '#daeaed'
+        backgroundColor: Colors.appBackGround
     },
     cententerTitle: {
-        color: '#000',
+        color: Colors.black,
         fontSize: 13,
-        fontFamily: 'Avenir-Medium',
+        fontFamily: Fonts.FONT_MEDIUM,
         paddingLeft: 30
     },
     errorMessage: {
-        color: '#f02424',
+        color: Colors.errorMessage,
         fontSize: 13,
-        fontFamily: 'Avenir-Medium',
+        fontFamily: Fonts.FONT_MEDIUM,
         paddingLeft: 30
     },
     textInput: {
@@ -44,13 +46,13 @@ export const styles = StyleSheet.create({
         marginTop: 12,
         borderWidth: 0.5,
         borderRadius: 5,
-        borderColor: '#718185',
+        borderColor: Colors.borderColor,
         height: 45,
         marginRight: 30
     },
     submit: {
         height: 50,
-        backgroundColor: '#8541B0',
+        backgroundColor: Colors.appColor,
         justifyContent: 'center',
         alignItems: 'center',
         marginHorizontal: 30,
@@ -58,14 +60,14 @@ export const styles = StyleSheet.create({
         marginTop: 40
     },
     submitTitle: {
-        color: '#fff',
+        color: Colors.white,
         fontSize: 16,
-        fontFamily: 'Avenir-Black'
+        fontFamily: Fonts.FONT_BLACK,
     },
     alreadySignInLabel: {
-        color: '#8541B0',
+        color: Colors.appColor,
         fontSize: 15,
-        fontFamily: 'Avenir-Black',
+        fontFamily: Fonts.FONT_BLACK,
         textAlign: 'center'
     },
 });

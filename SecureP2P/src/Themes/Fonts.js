336a33d30262665ec32d7019124c3531aac2f68d
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2020-05-24 01:18:43 +0530

    Generalise Fonts for styles

---
+++ SecureP2P/src/Themes/Fonts.js
@@ -0,0 +1,10 @@
+const fontFamily = {
+    FONT_BLACK: 'Avenir-Black',
+    FONT_BOOK: 'Avenir-Book',
+    FONT_LIGHT: 'Avenir-Light',
+    FONT_MEDIUM: 'Avenir-Medium',
+    FONT_OBLIQUE: 'Avenir-Oblique',
+    FONT_ROMAN: 'Avenir-Roman',
+};
+
+export default fontFamily;
